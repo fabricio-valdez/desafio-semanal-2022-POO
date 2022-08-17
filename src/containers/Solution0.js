fcf4f133a588e9523b26a7779bafae15b0a84b1c
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2022-08-17 08:07:24 -0300

    Beta

--- src/containers/Solution0.js
+++ src/containers/Solution0.js
@@ -1,3 +1,4 @@
+// 
 for (i = 1; i <= 100; i++) {
     let result = '';
     if (i%3 === 0) {
@@ -6,5 +7,5 @@ for (i = 1; i <= 100; i++) {
     if (i%5 === 0) {
         result += 'Buzz';
     }
-    console.log(result || i);
+    console.log(resultado || i);
 }

1240cb180bb69d0c8dee0872a72e606fd3763f05
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2022-08-31 09:22:10 -0300

    fix

--- src/containers/Solution1.js
+++
@@ -1,9 +0,0 @@
-function Anagrama (x,y) {
-    x = limpiar (x);
-    y + limpiar (y);
-    x = x.split ("").sort().join("");
-    y = y.split ("").sort().join("");
-}
-function limpiar (n) {
-    return n.replaceAll(" ","").toLowerCase()
-}

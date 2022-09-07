e8ada039b14128acaed5f3e177ae5b1c5bec7f9c
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2022-09-07 12:40:27 -0300

    fix

--- src/solutions/reto11.js
+++ src/solutions/reto11.js
@@ -7,12 +7,11 @@ function eliminarC (str1,str2){
         if (str1.indexOf(l1[i]) === str2.lastIndexOf(l1[i])){
             out1.push(l1[i]);
         }
-        if (str2.indexOf(l2[i]) === str1.lastIndexOf(l1[i])){
+        if (str2.indexOf(l2[i]) === str1.lastIndexOf(l2[i])){
             out2.push(l2[i]);
+        }
     }
     console.log(out1.join(''));
     console.log(out2.join(''));
 }
-}
 console.log(eliminarC('Hello World!','Hola Mundo!'))
-// Solucionar errores;
